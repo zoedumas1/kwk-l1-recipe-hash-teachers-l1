7c57501dfd38afaa29f09f5cf2ba145a0ad3dc5a
Zoe Dumas <zoe.dumas1@yahoo.com>
2018-07-12 12:40:54 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,16 +1,18 @@
 #code your solutions here
 
 #1. Create a hash called `perfect_10_recipe` that has the following data:
-#  - almond flour - 2 cups
-#  - gluten freewhole oats - 1 cup
-#  - kosher salt - 1/2 tea spoon
-#  - baking powder  - 1/2 tea spoon
-#  - baking soda - 1/4 tea spoon
-#  - Xanthan Gum - 1/2 tea spoon
-#  - slivered almonds, lightly toasted - 1/4 cup
-#  - mini dark chocolate chips - 3/4 cup
-#  - olive oil - 1/2 cup
-#  - agave - 1/4 cup
+perfect_10_ingredients = {
+     :almond_flour => "2 cups",
+     :gluten_freewhole_oats => "1 cup",
+     :kosher_salt => "1/2 tea spoon",
+     :baking_powder  => "1/2 tea spoon",
+     :baking_soda => "1/4 tea spoon",
+     :xanthan_Gum => "1/2 tea spoon",
+     :slivered_almonds => "1/4 cup",
+     :mini_dark_chocolate_chips => "3/4 cup",
+     :olive_oil => "1/2 cup",
+     :agave => "1/4 cup"
+   }
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
